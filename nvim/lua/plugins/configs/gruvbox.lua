7d0542a2830fafc430aff1cc4d203a5d0bfc9e1d
李天华 <litianhua@moresec.cn>
2022-01-05 11:51:18 +0800

    vim: telescope hi fix

--- nvim/lua/plugins/configs/gruvbox.lua
+++ nvim/lua/plugins/configs/gruvbox.lua
@@ -1,11 +1,10 @@
+local cmd = vim.cmd
 local g = vim.g
 local opt = vim.opt
 
 g.gruvbox_italics = 0
 
-vim.cmd([[colorscheme gruvbox8_hard]])
+cmd([[colorscheme gruvbox8_hard]])
 
-vim.cmd([[highlight link TelescopeSelection SignColumn]])
-vim.cmd([[highlight link TelescopePreviewLine SignColumn]])
-vim.cmd([[highlight CocExplorerFileDirectoryExpanded guifg=#8094b4]])
-vim.cmd([[highlight CocExplorerFileDirectoryCollapsed guifg=#8094b4]])
+cmd([[highlight CocExplorerFileDirectoryExpanded guifg=#8094b4]])
+cmd([[highlight CocExplorerFileDirectoryCollapsed guifg=#8094b4]])
