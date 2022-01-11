7ad4f804983d8665a5524eb042443e5e3df9f427
李天华 <litianhua@moresec.cn>
2022-01-11 20:14:28 +0800

    vim: wilder

--- nvim/lua/plugins/configs/wilder.lua
+++ nvim/lua/plugins/configs/wilder.lua
@@ -6,10 +6,14 @@ if not present then
   return
 end
 
-wilder.setup({ modes = { ":", "/", "?" }, enable_cmdline_enter = 1 })
+wilder.setup({ modes = { ":", "/", "?" } })
 
 wilder.set_option("pipeline", {
   wilder.branch(
+    -- TODO: 仅cmd模式
+    function(_, x)
+      return string.len(x) <= 2 and "" or false
+    end,
     wilder.substitute_pipeline({
       pipeline = wilder.python_search_pipeline({
         skip_cmdtype_check = 1,
@@ -31,13 +35,44 @@ wilder.set_option("pipeline", {
   ),
 })
 
-local hl_wilder_accent = wilder.make_hl("WilderAccent", "Search")
+local hl_wilder_accent = wilder.make_hl("WilderAccent", "Keyword")
+local scale = {
+  "#f4468f",
+  "#fd4a85",
+  "#ff507a",
+  "#ff566f",
+  "#ff5e63",
+  "#ff6658",
+  "#ff704e",
+  "#ff7a45",
+  "#ff843d",
+  "#ff9036",
+  "#f89b31",
+  "#efa72f",
+  "#e6b32e",
+  "#dcbe30",
+  "#d2c934",
+  "#c8d43a",
+  "#bfde43",
+  "#b6e84e",
+  "#aff05b",
+}
+local gradient = {}
+
+for i, v in ipairs(scale) do
+  table.insert(gradient, wilder.make_hl("Accent" .. i, "Keyword", { { foo = "" }, { foo = "" }, { foreground = v } }))
+end
 
 local wilder_popupmenu_renderer = wilder.popupmenu_renderer({
   highlights = {
-    accent = hl_wilder_accent,
+    -- accent = hl_wilder_accent,
+    gradient = gradient,
   },
-  highlighter = { wilder.pcre2_highlighter() },
+  highlighter = {
+    wilder.highlighter_with_gradient({
+      wilder.basic_highlighter(),
+    }),
+  },
   left = { " ", wilder.popupmenu_devicons() },
   right = { " ", wilder.popupmenu_scrollbar() },
 })
