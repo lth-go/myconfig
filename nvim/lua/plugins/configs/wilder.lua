a74a1d8322c624cd801d1413ed4913afc9e90e73
李天华 <litianhua@moresec.cn>
2022-01-12 18:41:40 +0800

    vim: wilder

--- nvim/lua/plugins/configs/wilder.lua
+++ nvim/lua/plugins/configs/wilder.lua
@@ -19,7 +19,7 @@ wilder.set_option("pipeline", {
       }),
     }),
     {
-      function(ctx, x)
+      function(_, x)
         if string.len(x) <= 2 then
           return false
         end
@@ -40,7 +40,28 @@ wilder.set_option("pipeline", {
   ),
 })
 
-local scale = { '#e1524a','#ec6449','#f3784c','#f88e53','#fba35e' }
+-- local scale = { '#e1524a','#ec6449','#f3784c','#f88e53','#fba35e' }
+local scale = {
+  "#f4468f",
+  "#fd4a85",
+  "#ff507a",
+  "#ff566f",
+  "#ff5e63",
+  "#ff6658",
+  "#ff704e",
+  "#ff7a45",
+  "#ff843d",
+  "#ff9036",
+  "#f89b31",
+  "#efa72f",
+  "#e6b32e",
+  "#dcbe30",
+  "#d2c934",
+  "#c8d43a",
+  "#bfde43",
+  "#b6e84e",
+  "#aff05b",
+}
 local gradient = {}
 
 for i, v in ipairs(scale) do
