45c146c7e763c21464538db70f2b8c6c4f8e49d2
李天华 <litianhua@moresec.cn>
2022-04-24 10:54:36 +0800

    vim: update

--- nvim/lua/plugins/configs/nvim_treesitter.lua
+++ nvim/lua/plugins/configs/nvim_treesitter.lua
@@ -8,6 +8,9 @@ ts_config.setup({
   ensure_installed = "maintained",
   highlight = {
     enable = true,
+    disable = function(lang, bufnr)
+      return vim.api.nvim_buf_line_count(bufnr) > 8192
+    end,
   },
   indent = {
     enable = false,
