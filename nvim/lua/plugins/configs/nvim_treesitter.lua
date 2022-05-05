68eab43e7151c3e7f247e35d07d60d5d03efc9d3
李天华 <litianhua@moresec.cn>
2022-05-05 10:44:04 +0800

    vim: update

--- nvim/lua/plugins/configs/nvim_treesitter.lua
+++ nvim/lua/plugins/configs/nvim_treesitter.lua
@@ -5,7 +5,7 @@ if not present then
 end
 
 ts_config.setup({
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   highlight = {
     enable = true,
     disable = function(lang, bufnr)
