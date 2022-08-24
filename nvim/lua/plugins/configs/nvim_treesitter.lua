71394a29403fedde54353734bd49634251ca074a
李天华 <litianhua@moresec.cn>
2022-08-24 16:49:34 +0800

    vim: update

--- nvim/lua/plugins/configs/nvim_treesitter.lua
+++ nvim/lua/plugins/configs/nvim_treesitter.lua
@@ -9,6 +9,11 @@ treesitter.setup({
   highlight = {
     enable = true,
     disable = function(lang, bufnr)
+      -- 渲染有问题
+      if lang == "dockerfile" then
+        return true
+      end
+
       return vim.api.nvim_buf_line_count(bufnr) > 8192
     end,
     use_languagetree = true,
