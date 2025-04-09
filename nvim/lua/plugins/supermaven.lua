a908757bbab401b2d28021a275a04df606b25a54
李天华 <litianhua@moresec.cn>
2025-04-09 12:32:14 +0800

    vim: AI

--- nvim/lua/plugins/supermaven.lua
+++ nvim/lua/plugins/supermaven.lua
@@ -13,39 +13,12 @@ local file_types = {
   "yaml",
 }
 
-local hijack = function()
-  local binary = require("supermaven-nvim.binary.binary_handler")
-  local old_on_update = binary.on_update
-
-  binary.on_update = function(self, buffer, file_name, event_type)
-    if not vim.tbl_contains(file_types, vim.bo.filetype) then
-      return
-    end
-
-    old_on_update(self, buffer, file_name, event_type)
-  end
-end
-
 return {
-  "supermaven-inc/supermaven-nvim",
+  "lth-go/supermaven-nvim",
   ft = file_types,
   config = function()
-    hijack()
-
     require("supermaven-nvim").setup({
-      keymaps = {
-        accept_suggestion = "<C-j>",
-        clear_suggestion = "<C-]>",
-        accept_word = "<A-j>",
-      },
-      ignore_filetypes = {},
-      color = {
-        suggestion_color = "#928374",
-        cterm = 245,
-      },
-      log_level = "error",
-      disable_inline_completion = false,
-      disable_keymaps = false,
+      filetypes = file_types,
     })
   end,
 }
